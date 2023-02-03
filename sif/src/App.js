9999a1eb9c3e0b3720f65898e0e4fa6626538758
Sanjeev Devarajan <realsanjeevd@gmail.com>
2023-02-03 01:11:58 -0500

    working on the user input for the data page

--- sif/src/App.js
+++ sif/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./components/TextBox.css";
 
-function App() {
+const TextBox = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [words, setWords] = useState(["apple", "banana", "cherry", "date"]);
+  const [filteredWords, setFilteredWords] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+    setFilteredWords(
+      words.filter((word) => word.startsWith(inputValue))
+    );
+  };
+
+  const [inputs, setInputs] = useState([]);
+  
+  const handleKeyUp = (event) => {
+    if (event.keyCode === 13) {
+      setInputs([...inputs, event.target.value]);
+      event.target.value = "";
+    }
+  }
+  
+  const handleCloseClick = (index) => {
+    setInputs(inputs.filter((input, i) => i !== index));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="textbox-container">
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyUp={handleKeyUp} 
+      />
+
+      {inputs.map((input, index) => (
+        <div key={index} style={{width: "300px", height: "50px", border: "1px solid black", padding: "10px", fontSize: "20px", marginBottom: "10px", position: "relative"}}>
+          {input}
+          <span style={{position: "absolute", top: "5px", right: "5px", cursor: "pointer", fontSize: "20px"}} onClick={() => handleCloseClick(index)}>x</span>
+        </div>
+      ))}
+
+      {filteredWords.length > 0 && (
+        <ul className="filtered-words">
+          {filteredWords.map((word) => (
+            <p key={word}>{word}</p>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default TextBox;
